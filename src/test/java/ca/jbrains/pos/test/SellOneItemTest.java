fef9921700288f57e2feaf7e32447089d42bf5d2
J. B. Rainsberger <me@jbrains.ca>
2016-11-14 16:49:02 +0100

    We now have a lookup table for mapping barcodes to prices.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -3,6 +3,9 @@ package ca.jbrains.pos.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
@@ -32,10 +35,16 @@ public class SellOneItemTest {
         }
 
         public void onBarcode(String barcode) {
+            final Map<String, String> pricesByBarcode
+                    = new HashMap<String, String>() {{
+                put("12345", "EUR 7.50");
+                put("23456", "EUR 12.95");
+            }};
+
             if ("12345".equals(barcode))
-                display.setText("EUR 7.50");
-            else
-                display.setText("EUR 12.95");
+                display.setText(pricesByBarcode.get(barcode));
+            else if ("23456".equals(barcode))
+                display.setText(pricesByBarcode.get(barcode));
         }
     }
 
