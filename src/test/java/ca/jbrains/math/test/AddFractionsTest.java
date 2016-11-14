f5d72f8c951ce0ed545166de5a1c13a304c15dd3
J. B. Rainsberger <me@jbrains.ca>
2016-11-14 13:45:49 +0100

    Removed obsolete assertions.

--- src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -8,64 +8,48 @@ public class AddFractionsTest {
     @Test
     public void zeroPlusZero() throws Exception {
         Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assert.assertEquals(0, sum.getNumerator());
-        Assert.assertEquals(1, sum.getDenominator());
         Assert.assertEquals(new Fraction(0), sum);
     }
 
     @Test
     public void integerNotZeroPlusZero() throws Exception {
         Fraction sum = new Fraction(3).plus(new Fraction(0));
-        Assert.assertEquals(3, sum.getNumerator());
-        Assert.assertEquals(1, sum.getDenominator());
         Assert.assertEquals(new Fraction(3), sum);
     }
 
     @Test
     public void zeroPlusIntegerNotZero() throws Exception {
         Fraction sum = new Fraction(0).plus(new Fraction(7));
-        Assert.assertEquals(7, sum.getNumerator());
-        Assert.assertEquals(1, sum.getDenominator());
         Assert.assertEquals(new Fraction(7), sum);
     }
 
     @Test
     public void bothNotZeroIntegers() throws Exception {
         Fraction sum = new Fraction(4).plus(new Fraction(8));
-        Assert.assertEquals(12, sum.getNumerator());
-        Assert.assertEquals(1, sum.getDenominator());
         Assert.assertEquals(new Fraction(12), sum);
     }
 
     @Test
     public void negativeIntegers() throws Exception {
         Fraction sum = new Fraction(-9).plus(new Fraction(11));
-        Assert.assertEquals(2, sum.getNumerator());
-        Assert.assertEquals(1, sum.getDenominator());
         Assert.assertEquals(new Fraction(2), sum);
     }
 
     @Test
     public void nonIntegerNotZeroPlusZero() throws Exception {
         Fraction sum = new Fraction(3, 4).plus(new Fraction(0));
-        Assert.assertEquals(3, sum.getNumerator());
-        Assert.assertEquals(4, sum.getDenominator());
         Assert.assertEquals(new Fraction(3, 4), sum);
     }
 
     @Test
     public void zeroPlusNonIntegerNotZero() throws Exception {
         Fraction sum = new Fraction(0).plus(new Fraction(5, 7));
-        Assert.assertEquals(5, sum.getNumerator());
-        Assert.assertEquals(7, sum.getDenominator());
         Assert.assertEquals(new Fraction(5, 7), sum);
     }
 
     @Test
     public void bothNotZeroWithSameDenominator() throws Exception {
         Fraction sum = new Fraction(1, 7).plus(new Fraction(5, 7));
-        Assert.assertEquals(6, sum.getNumerator());
-        Assert.assertEquals(7, sum.getDenominator());
         Assert.assertEquals(new Fraction(6, 7), sum);
     }
 
@@ -74,8 +58,6 @@ public class AddFractionsTest {
         Fraction sum = new Fraction(1, 2)
                 .plus(new Fraction(4, 3));
 
-        Assert.assertEquals(11, sum.getNumerator());
-        Assert.assertEquals(6, sum.getDenominator());
         Assert.assertEquals(new Fraction(11, 6), sum);
     }
 
