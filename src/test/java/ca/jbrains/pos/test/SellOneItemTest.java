489f77a8947d8c356eb85d6f09df1de8e04ab170
J. B. Rainsberger <me@jbrains.ca>
2016-11-15 13:36:51 +0100

    Introduced a new abstraction for the Model.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -4,16 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
             put("12345", "EUR 7.50");
             put("23456", "EUR 12.95");
-        }});
+        }}));
 
         sale.onBarcode("12345");
 
@@ -23,10 +22,10 @@ public class SellOneItemTest {
     @Test
     public void anotherProductFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
             put("12345", "EUR 7.50");
             put("23456", "EUR 12.95");
-        }});
+        }}));
 
         sale.onBarcode("23456");
 
@@ -36,10 +35,10 @@ public class SellOneItemTest {
     @Test
     public void productNotFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
             put("12345", "EUR 7.50");
             put("23456", "EUR 12.95");
-        }});
+        }}));
 
         sale.onBarcode("99999");
 
@@ -52,10 +51,10 @@ public class SellOneItemTest {
     @Test
     public void emptyBarcode() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
             put("12345", "EUR 7.50");
             put("23456", "EUR 12.95");
-        }});
+        }}));
 
         sale.onBarcode("");
 
@@ -66,12 +65,12 @@ public class SellOneItemTest {
     }
 
     public static class Sale {
-        private final Map<String, String> pricesByBarcode;
+        private final Catalog catalog;
         private Display display;
 
-        public Sale(Display display, Map<String, String> pricesByBarcode) {
+        public Sale(Display display, Catalog catalog) {
             this.display = display;
-            this.pricesByBarcode = pricesByBarcode;
+            this.catalog = catalog;
         }
 
         public void onBarcode(String barcode) {
@@ -80,16 +79,13 @@ public class SellOneItemTest {
                 return;
             }
 
-            final String priceAsText = findPrice(barcode);
+            final String priceAsText = catalog.findPrice(barcode);
             if (priceAsText == null)
                 display.displayProductNotFoundMessage(barcode);
             else
                 display.displayPrice(priceAsText);
         }
 
-        private String findPrice(String barcode) {
-            return pricesByBarcode.get(barcode);
-        }
     }
 
     public static class Display {
