416c2df6a96d16c34a0878de9b5c18c6f7795d3d
J. B. Rainsberger <me@jbrains.ca>
2016-11-15 16:31:31 +0100

    We can find prices in an in-memory Catalog.

---
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -0,0 +1,35 @@
+package ca.jbrains.pos.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class FindPriceInMemoryCatalogTest {
+    @Test
+    public void productFound() throws Exception {
+        final Price foundPrice = Price.cents(1250);
+        final InMemoryCatalog catalog = new InMemoryCatalog(Collections.singletonMap(
+                "12345", foundPrice));
+        Assert.assertEquals(foundPrice, catalog.findPrice("12345"));
+    }
+
+    @Test
+    public void productNotFound() throws Exception {
+        final InMemoryCatalog catalog = new InMemoryCatalog(Collections.emptyMap());
+        Assert.assertEquals(null, catalog.findPrice("12345"));
+    }
+
+    public static class InMemoryCatalog {
+        private final Map<String, Price> pricesByBarcode;
+
+        public InMemoryCatalog(Map<String, Price> pricesByBarcode) {
+            this.pricesByBarcode = pricesByBarcode;
+        }
+
+        public Price findPrice(String barcode) {
+            return pricesByBarcode.get(barcode);
+        }
+    }
+}
