a2982788b7fb5496b0c7e06f310c624d95993042
J. B. Rainsberger <me@jbrains.ca>
2016-11-16 15:24:23 +0100

    We are prepared to split some behavior into smaller pieces.

--- src/test/java/ca/jbrains/pos/test/TextCommandConsumer.java
+++ src/test/java/ca/jbrains/pos/test/TextCommandConsumer.java
@@ -2,7 +2,9 @@ package ca.jbrains.pos.test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
+import java.util.stream.Stream;
 
 class TextCommandConsumer {
     private final BarcodeScannedListener barcodeScannedListener;
@@ -11,10 +13,18 @@ class TextCommandConsumer {
         this.barcodeScannedListener = barcodeScannedListener;
     }
 
-    public void consumeBarcodeCommands(StringReader commandSource) {
-        new BufferedReader(commandSource).lines()
-                .map(String::trim)
-                .filter((line) -> !line.isEmpty())
+    public void consumeBarcodeCommands(Reader commandSource) {
+        sanitizeCommandStream(commandStream(commandSource))
                 .forEach(barcodeScannedListener::onBarcode);
+    }
+
+    private Stream<String> sanitizeCommandStream(Stream<String> commandStream) {
+        return commandStream
+                .map(String::trim)
+                .filter((line) -> !line.isEmpty());
+    }
+
+    private Stream<String> commandStream(Reader commandSource) {
+        return new BufferedReader(commandSource).lines();
     }
 }
