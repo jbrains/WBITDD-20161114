1f3f09154797d9618189d163c2c338d90c028247
J. B. Rainsberger <me@jbrains.ca>
2016-11-16 12:19:57 +0100

    Identified a test that we might not need.

--- src/test/groovy/ca/jbrains/pos/test/RenderToPrintWriterTest.groovy
+++ src/test/groovy/ca/jbrains/pos/test/RenderToPrintWriterTest.groovy
@@ -2,7 +2,8 @@ package ca.jbrains.pos.test
 
 import spock.lang.Specification
 
-
+// This test doesn't care what you're rendering!
+// PrintWriterCanvas might be Too Simple to Break.
 class RenderToPrintWriterTest extends Specification {
     def "happy path"() {
         expect:
