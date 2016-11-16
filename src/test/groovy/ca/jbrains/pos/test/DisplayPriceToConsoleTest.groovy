0bcba30bea4f42056e76521dbe559f2dc39ee1c4
J. B. Rainsberger <me@jbrains.ca>
2016-11-16 09:56:50 +0100

    We can now display any price that interests us.

--- src/test/groovy/ca/jbrains/pos/test/DisplayPriceToConsoleTest.groovy
+++ src/test/groovy/ca/jbrains/pos/test/DisplayPriceToConsoleTest.groovy
@@ -29,8 +29,15 @@ class DisplayPriceToConsoleTest extends Specification {
         displayText == canvas.toString("UTF-8").trim()
 
         where:
-        price            || displayText
-        Price.cents(175) || "EUR 1.75"
-        Price.cents(230) || "EUR 2.30"
+        price                  || displayText
+        Price.cents(175)       || "EUR 1.75"
+        Price.cents(230)       || "EUR 2.30"
+        Price.cents(0)         || "EUR 0.00"
+        Price.cents(1)         || "EUR 0.01"
+        Price.cents(10)        || "EUR 0.10"
+        Price.cents(99)        || "EUR 0.99"
+        Price.cents(1000)      || "EUR 10.00"
+        Price.cents(23746)     || "EUR 237.46"
+        Price.cents(234743341) || "EUR 2347433.41"
     }
 }
